e022ea22a5a6093c9ed58bb350caae1f6c17c02d
Raphael Villas <raphael.villas@gmail.com>
2017-09-01 18:05:44 -0500

    Initial react src for rendering basic text on the page

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component to produce some html
+const App = function () {   // const means that this is the final value. Here we are making a component.
+  return <div>Hello!</div>; // This is JSX, which gets transpiled to javascript.
+                            // Test here: http://babeljs.io/repl
+}
+// Put the component HTML into the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));   // To make an instance of the App class, we wrap it in a JSX tag
+
