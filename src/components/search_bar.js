2cb117bd56c5508c4abf885c6b8ed4d4c043462a
Raphael Villas <raphael.villas@gmail.com>
2017-09-01 23:34:54 -0500

    Refactor to ES6 syntactic sugar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 //class-based component (ES6)
-class SearchBar extends React.Component {
+class SearchBar extends Component {
   render() { //method definition in ES6
     return <input />;
   }
