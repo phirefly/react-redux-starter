e40c302a81525957b523d07f287dab6bad0d4381
Raphael Villas <raphael.villas@gmail.com>
2017-09-01 23:49:53 -0500

    Initialize state in class-based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react'
 
 //class-based component (ES6)
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' }
+  }
+
   render() { //method definition in ES6
     return <input onChange={event => console.log(event.target.value)} />;
   }
