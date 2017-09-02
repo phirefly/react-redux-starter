1279316c5ba7b25e708cd210156c74396cbd5161
Raphael Villas <raphael.villas@gmail.com>
2017-09-01 23:42:45 -0500

    Added event handler to the text field

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 //class-based component (ES6)
 class SearchBar extends Component {
   render() { //method definition in ES6
-    return <input />;
+    return <input onChange={this.onInputChange} />;
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
 
