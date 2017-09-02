7b6d7c9aafb4f682ab4df39a04a6be785c951f93
Raphael Villas <raphael.villas@gmail.com>
2017-09-01 23:46:09 -0500

    Refactor to ES6 arrow functions to make things a bit more concise

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,12 +3,9 @@ import React, { Component } from 'react'
 //class-based component (ES6)
 class SearchBar extends Component {
   render() { //method definition in ES6
-    return <input onChange={this.onInputChange} />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
 }
 
 export default SearchBar;
